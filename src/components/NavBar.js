3ef64e6b32b1cd7216ef400970ee0dbe49c12bd2
unknown <ianesposo3125@gmail.com>
2021-08-13 03:54:40 +0800

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,13 +10,13 @@ export default function NavBar() {
                     <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Ian
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink to="/post" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink to="/project" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink to="/about" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         About Me!
                     </NavLink>
                 </nav>
